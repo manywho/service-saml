91222aff0feab172a019100668d3165ff831381b
Bates <Matthew_Bates@Dell.com>
2020-01-23 15:20:36 +0000

    Added test for group authorization fix

--- src/test/java/com/manywho/services/saml/SamlServiceTest.java
+++ src/test/java/com/manywho/services/saml/SamlServiceTest.java
@@ -1,10 +1,23 @@
 package com.manywho.services.saml;
 
+import com.manywho.sdk.enums.AuthenticationType;
+import com.manywho.sdk.entities.run.elements.config.Authorization;
+import com.manywho.sdk.entities.run.elements.config.Group;
+import com.manywho.sdk.entities.run.elements.config.GroupCollection;
+import com.manywho.sdk.entities.security.AuthenticatedWho;
+import com.manywho.services.saml.managers.CacheManager;
+import com.manywho.services.saml.services.AuthorizationService;
 import com.manywho.services.saml.services.JwtService;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class SamlServiceTest {
     @Test
@@ -30,5 +43,29 @@ public class SamlServiceTest {
         String token = service.sign("123456", LocalDateTime.now().plusSeconds(10), LocalDateTime.now().plusSeconds(20));
 
         Assert.assertFalse(service.isValid(token));
+    }
+
+    @Test
+    public void testGetStatusSpecifiedFlow200GroupAuthorized() throws Exception {
+        CacheManager cacheManager = mock(CacheManager.class);
+        JwtService jwtService = mock(JwtService.class);
+        when(jwtService.isValid(any()))
+                .thenReturn(true);
+        ArrayList<String> userGroups = new ArrayList<>();
+        userGroups.add("group-test");
+        when(cacheManager.getUserGroups("user-test"))
+                .thenReturn(userGroups);
+        AuthorizationService authorizationService = new AuthorizationService(cacheManager, jwtService);
+        Authorization authorization = new Authorization();
+        AuthenticatedWho authenticatedWho = new AuthenticatedWho();
+        authenticatedWho.setUserId("user-test");
+        authorization.setGlobalAuthenticationType(AuthenticationType.Specified);
+        Group groupTest = new Group();
+        groupTest.setAuthenticationId("group-test");
+        GroupCollection groups = new GroupCollection();
+        groups.add(groupTest);
+        authorization.setGroups(groups);
+        String actualStatus = authorizationService.getStatus(authorization, authenticatedWho);
+        Assert.assertEquals("200", actualStatus);
     }
 }
